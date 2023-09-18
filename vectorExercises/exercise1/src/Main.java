ea4798575ff9085674eb14d4a87b8af4e45574ae
Lucas Ferreira <ferreiravpl@gmail.com>
2023-09-18 20:08:41 -0300

    adding the vect exercise

--- vectorExercises/exercise1/src/Main.java
+++ vectorExercises/exercise1/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantos números você vai digitar? ");
+        int qtVetor = sc.nextInt();
+        double[] vect = new double[qtVetor];
+
+        for (int numero = 0; numero < vect.length; numero++) {
+            System.out.print("Digite um número: ");
+            vect[numero] = sc.nextDouble();
+        }
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i] < 0) {
+                System.out.println("Numeros negativos: " + vect[i]);
+            } sc.close();
+        }
     }
 }
