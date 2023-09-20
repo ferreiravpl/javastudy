f739bd8d732fc3d10a346d51f3b6d8e06ef9b78e
Lucas Ferreira <ferreiravpl@gmail.com>
2023-09-20 13:06:36 -0300

    criando primeiro laço de repetição para questionar a qtd de pessoas

--- vectorExercises/exercise3/src/Main.java
+++ vectorExercises/exercise3/src/Main.java
@@ -1,3 +1,5 @@
+import entities.Pessoa;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -6,6 +8,22 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
+
+        System.out.print("Quanta pessoas serão digitadas? ");
+        int qtdPessoas = sc.nextInt();
+        double[] vect = new double[qtdPessoas];
+
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println("Dados da " + vect[i] + "a pessoa:");
+            System.out.print("Nome: ");
+            String nome = sc.nextLine();
+            sc.nextLine();
+            System.out.print("Idade: ");
+            int idade = sc.nextInt();
+            System.out.print("Altura: ");
+            double altura = sc.nextDouble();
+            Pessoa pessoa = new Pessoa(nome, idade, altura);
+        }
 
 
         sc.close();
