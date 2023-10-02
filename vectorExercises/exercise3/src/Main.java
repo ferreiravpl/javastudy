3d5c158e5afbbd9e968bdee690a4cc40a0110a0d
Lucas Ferreira <ferreiravpl@gmail.com>
2023-10-02 20:14:29 -0300

    criando laços de porcentagem de menores de idade e média de altura

--- vectorExercises/exercise3/src/Main.java
+++ vectorExercises/exercise3/src/Main.java
@@ -11,20 +11,51 @@ public class Main {
 
         System.out.print("Quanta pessoas serão digitadas? ");
         int qtdPessoas = sc.nextInt();
-        double[] vect = new double[qtdPessoas];
+        Pessoa[] vect = new Pessoa[qtdPessoas];
 
         for (int i = 0; i < vect.length; i++) {
-            System.out.println("Dados da " + vect[i] + "a pessoa:");
+            System.out.println("Dados da " + i + "a pessoa:");
+            sc.nextLine();
             System.out.print("Nome: ");
             String nome = sc.nextLine();
-            sc.nextLine();
             System.out.print("Idade: ");
             int idade = sc.nextInt();
             System.out.print("Altura: ");
             double altura = sc.nextDouble();
-            Pessoa pessoa = new Pessoa(nome, idade, altura);
+            vect[i] = new Pessoa(nome, idade, altura);
         }
 
+        double mediaAltura = 0.0;
+
+        for (int i = 0; i < vect.length; i++) {
+            mediaAltura += vect[i].getAltura() / qtdPessoas;
+        }
+
+        System.out.println("Média de altura: " + mediaAltura);
+
+        double porcentagemIdade = 0.0;
+        int contadorMenoresDe16 = 0;
+
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i].getIdade() < 16) {
+              contadorMenoresDe16++;
+            }
+        }
+
+        if (contadorMenoresDe16 > 0) {
+            porcentagemIdade = ((double)contadorMenoresDe16 / qtdPessoas * 100.00);
+            System.out.println("Porcentagem de pessoas menores de 16 anos: " + porcentagemIdade);
+
+        } else {
+            System.out.println("Não existem pessoas menores de 16 anos");
+        }
+
+        for (int i = 0; i < vect.length; i++) {
+            if (vect[i].getIdade() < 16) {
+                System.out.println("Nome das pessoas:");
+                System.out.printf("%s\n", vect[i].getNome());
+            }
+        }
 
         sc.close();
     }
