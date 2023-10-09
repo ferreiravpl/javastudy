90b5547d1c68730988ad09120076061d096b846c
Lucas Ferreira <ferreiravpl@gmail.com>
2023-10-09 19:11:16 -0300

    Created busyRooms validator

--- vectorExercises/exercise5/src/application/Program.java
+++ vectorExercises/exercise5/src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 import entities.Renter;
+
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,10 +11,10 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        //Criação do array de quartos totais
+        //Criando array de quartos totais
         double[] rentableRooms = new double[9];
 
-        //Criação do array de quartos que serão alugados
+        //Criando array de quartos que serão alugados
         System.out.print("How many roms will be rented? ");
         int roomsToRent = sc.nextInt();
         Renter[] rentedRooms = new Renter[roomsToRent];
@@ -27,6 +29,20 @@ public class Program {
             System.out.print("Room: ");
             int roomNumber = sc.nextInt();
             rentedRooms[i] = new Renter(renterName, renterEmail, roomNumber);
+        }
+
+        //Validação de quais quartos estão alugados em ordem crescente
+        int busyRooms = 0;
+        System.out.println("Busy rooms:");
+
+        for (int i = 0; i < rentedRooms.length; i++) {
+            if (rentedRooms[i] != null) {
+                System.out.println(rentedRooms[i].getRoomNumber() + ": " + rentedRooms[i].getName() + ", " + rentedRooms[i].getEmail());
+                busyRooms++;
+            }
+            else {
+                System.out.println("No rented rooms.");
+            }
         }
 
         sc.close();
