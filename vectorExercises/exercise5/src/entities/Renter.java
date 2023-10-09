c16a0491f5670f590b3b8886f933aa4787f2ac58
Lucas Ferreira <ferreiravpl@gmail.com>
2023-10-09 19:24:40 -0300

    Commenting unused methods

--- vectorExercises/exercise5/src/entities/Renter.java
+++ vectorExercises/exercise5/src/entities/Renter.java
@@ -16,24 +16,24 @@ public class Renter {
         return roomNumber;
     }
 
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
+//    public void setRoomNumber(int roomNumber) {
+//        this.roomNumber = roomNumber;
+//    }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
 
 }
