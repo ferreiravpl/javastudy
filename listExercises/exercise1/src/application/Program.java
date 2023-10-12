8b9864d9790417883586789420b9f108a64b5862
Lucas Ferreira <ferreiravpl@gmail.com>
2023-10-12 12:11:13 -0300

    Criando classe Program com o método main e início do projeto

---
+++ listExercises/exercise1/src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+import entities.Employee;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        sc.close();
+
+    }
+}
