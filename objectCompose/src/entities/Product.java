146d8cf1d0525b697e92392d697f0304755b7fb6
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-19 19:55:55 -0300

    Ajustando construtor da Product

--- objectCompose/src/entities/Product.java
+++ objectCompose/src/entities/Product.java
@@ -9,7 +9,7 @@ public class Product {
 
     }
 
-    public Product (String name, Double price, OrderItem item) {
+    public Product (String name, Double price) {
         this.name = name;
         this.price = price;
     }
