ffbbe7a4b73f0d8856de101be8aac676adf48950
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-19 19:57:14 -0300

    Criando construtores da classe, acesso à lista de items/cliente e ajuste do método total

--- objectCompose/src/entities/Order.java
+++ objectCompose/src/entities/Order.java
@@ -4,16 +4,40 @@ import entities.enums.OrderStatus;
 import java.util.ArrayList;
 import java.util.List;
 import entities.OrderItem;
-
-
 import java.util.Date;
 
 public class Order {
 
     private Date moment;
     private OrderStatus status;
+    private Client client;
+    private List<OrderItem> orderItemList = new ArrayList<OrderItem>();
 
-    List<OrderItem> orderItemList = new ArrayList<>();
+    public Order () {
+
+    }
+
+    public Order (Date moment, OrderStatus status, Client client) {
+        this.moment = moment;
+        this.status = status;
+        this.client = client;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 
     public void addItem (OrderItem item) {
         orderItemList.add(item);
@@ -23,10 +47,19 @@ public class Order {
         orderItemList.remove(item);
     }
 
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
     public Double total () {
+        double sum = 0;
         for (OrderItem oi : orderItemList) {
-            return oi.subTotal();
+           sum += oi.subTotal();
         }
-        return null;
+        return sum;
     }
 }
