a9ab6e763502c88edf236b8e85192ceeeeffdb13
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-19 16:09:01 -0300

    Criando classe OrderItem

---
+++ objectCompose/src/entities/OrderItem.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class OrderItem {
+
+    private Integer quantity;
+    private Double price;
+
+    public OrderItem () {
+
+    }
+
+    public OrderItem (Integer quantity, Double price) {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Double subTotal () {
+        return quantity * price;
+    }
+
+}
