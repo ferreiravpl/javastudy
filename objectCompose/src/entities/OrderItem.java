a4b94b3f54e7da62a8bc602f4a70d832b5835688
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-19 19:55:19 -0300

    Criando acesso ao produto pelo orderItem e ajustando o toString

--- objectCompose/src/entities/OrderItem.java
+++ objectCompose/src/entities/OrderItem.java
@@ -2,20 +2,56 @@ package entities;
 
 public class OrderItem {
 
-    private Integer quantity;
-    private Double price;
+    private static Integer quantity;
+    private static Double price;
+    private Product product;
 
     public OrderItem () {
 
     }
 
-    public OrderItem (Integer quantity, Double price) {
+    public OrderItem (Integer quantity, Double price, Product product) {
         this.quantity = quantity;
         this.price = price;
+        this.product = product;
     }
 
-    public Double subTotal () {
+    public static Integer getQuantity() {
+        return quantity;
+    }
+
+    public static void setQuantity(Integer quantity) {
+        OrderItem.quantity = quantity;
+    }
+
+    public static Double getPrice() {
+        return price;
+    }
+
+    public static void setPrice(Double price) {
+        OrderItem.price = price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public static Double subTotal () {
         return quantity * price;
     }
 
+    @Override
+    public String toString() {
+        return getProduct().getName()
+                + ", $"
+                + String.format("%.2f", price)
+                + ", Quantity: "
+                + quantity
+                + ", Subtotal: $"
+                + String.format("%.2f", subTotal());
+    }
 }
