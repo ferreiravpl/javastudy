fad5fb0a9debb99c4f714acb2c6169844db9dfd2
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-26 16:25:16 -0300

    Print do objeto order adicionado

--- objectCompose/src/application/Program.java
+++ objectCompose/src/application/Program.java
@@ -3,19 +3,18 @@ package application;
 import entities.Client;
 import entities.OrderItem;
 import entities.Product;
+import entities.Order;
+import entities.enums.OrderStatus;
 
-import java.sql.SQLOutput;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         System.out.println("Enter client data:");
         System.out.print("Name: ");
@@ -26,23 +25,29 @@ public class Program {
         Date birthDate = sdf.parse(sc.next());
         Client client = new Client(name, email, birthDate);
         System.out.println("Enter order data:");
+        sc.nextLine();
         System.out.print("Status: ");
-        String orderStatus = sc.nextLine();
+        OrderStatus orderStatus = OrderStatus.valueOf(sc.next());
+        Order order = new Order(new Date(), orderStatus, client);
         System.out.print("How many items to this order? ");
         Integer itemsQty = sc.nextInt();
         sc.nextLine();
 
         for (int i = 1; i <= itemsQty; i++) {
             System.out.println("Enter #" + i + " item data:");
-            System.out.print("Product name");
+            System.out.print("Product name: ");
             String productName = sc.nextLine();
-            System.out.print("Product price");
+            System.out.print("Product price: ");
             Double productPrice = sc.nextDouble();
             System.out.print("Quantity: ");
             Integer productQuantity = sc.nextInt();
-            Product p = new Product (productName, productPrice, new OrderItem(productQuantity, productPrice));
-
+            Product product = new Product (productName, productPrice);
+            OrderItem orderItem = new OrderItem (productQuantity, productPrice, product);
+            order.addItem(orderItem);
+            sc.nextLine();
         }
+        System.out.println();
+        System.out.println(order);
 
         sc.close();
     }
