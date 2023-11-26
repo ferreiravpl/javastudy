b582b5b6f4b412017ae0bcafb515f950635726da
Lucas Ferreira <ferreiravpl@gmail.com>
2023-11-26 16:15:50 -0300

    Criando StringBuilder e SimpleDateFormat

--- objectCompose/src/entities/Order.java
+++ objectCompose/src/entities/Order.java
@@ -1,9 +1,10 @@
 package entities;
 
 import entities.enums.OrderStatus;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import entities.OrderItem;
 import java.util.Date;
 
 public class Order {
@@ -11,6 +12,7 @@ public class Order {
     private Date moment;
     private OrderStatus status;
     private Client client;
+    private SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
     private List<OrderItem> orderItemList = new ArrayList<OrderItem>();
 
     public Order () {
@@ -61,5 +63,22 @@ public class Order {
            sum += oi.subTotal();
         }
         return sum;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Order moment: ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append("Order status: ");
+        sb.append(status + "\n");
+        sb.append("Client: ");
+        sb.append(client + "\n");
+        sb.append("Order items:\n");
+        for (OrderItem item : orderItemList) {
+            sb.append(item + "\n");
+        }
+        sb.append("Total price: $");
+        sb.append(String.format("%.2f", total()));
+        return sb.toString();
     }
 }
