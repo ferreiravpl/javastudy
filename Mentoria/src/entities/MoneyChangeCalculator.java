8d78931c1947de3a97acfa2e3a6f7a40fa9f3a3b
Lucas Ferreira <ferreiravpl@gmail.com>
2023-12-09 11:44:59 -0300

    Criada lista e método para retorno do troco

--- Mentoria/src/entities/MoneyChangeCalculator.java
+++ Mentoria/src/entities/MoneyChangeCalculator.java
@@ -5,13 +5,47 @@ import java.util.List;
 
 public class MoneyChangeCalculator extends Transactions {
 
-    public List<Double> noteList = new ArrayList<>();
+    double returnTotalNoteChange;
 
-    public MoneyChangeCalculator() {
+    List noteList = new ArrayList();
 
+    //Método para inserir as cédulas na lista
+    public void setNoteList() {
+        noteList.add(100.0);
+        noteList.add(50.0);
+        noteList.add(25.0);
+        noteList.add(10.0);
+        noteList.add(5.0);
+        noteList.add(2.0);
+        noteList.add(1.0);
+        noteList.add(0.50);
+        noteList.add(0.25);
+        noteList.add(0.10);
+        noteList.add(0.05);
     }
 
-    public Double calculateMoneyChange () {
+    //Método para consultar a lista
+    public List<Double> returnNoteList () {
+        return noteList;
+    }
+
+    //Método para calcular o troco em cédulas
+
+    public double returnTotalNoteChange () {
+        for (int i = 0; i < noteList.size(); i++) {
+            noteList.get(i);
+            returnTotalNoteChange = calculateTotalMoneyChange() / i;
+            if (returnTotalNoteChange != 0) {
+                System.out.println(returnTotalNoteChange);
+            } else {
+                System.out.println("O troco está completo");
+            }
+        }
+        return returnTotalNoteChange;
+    }
+
+    //Calculando o valor total do troco (sem separação por cédulas)
+    public Double calculateTotalMoneyChange () {
         return Transactions.getPaidAmount() - Transactions.getCost();
     }
 
