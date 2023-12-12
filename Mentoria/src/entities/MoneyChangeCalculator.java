4ceaf9d8382bfaab7f001c32ba77d2863a7f1148
Lucas Ferreira <ferreiravpl@gmail.com>
2023-12-12 14:27:42 -0300

    Limpando o código, criando as variáveis fora do método e retirando algumas anotações

--- Mentoria/src/entities/MoneyChangeCalculator.java
+++ Mentoria/src/entities/MoneyChangeCalculator.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 public class MoneyChangeCalculator extends Transactions {
 
-    double returnTotalNoteChange = calculateTotalMoneyChange();
-    double newValueNoteChange;
+    Double returnTotalNoteChange = calculateTotalMoneyChange();
+    Double newValueNoteChange;
+    Integer eachValueNoteQuantity = (int) Math.floor(returnTotalNoteChange);
 
     List<Double> noteList = new ArrayList();
 
@@ -32,51 +33,26 @@ public class MoneyChangeCalculator extends Transactions {
         return noteList;
     }
 
-    //Desordenar lista chumbada e ordenar em um próprio método
     //Ajustar variável returnTotalNoteChange pq tá pegando sempre o troco total e não o resto das próximas divisões;
-    //Criar validação pra que somente aceite o pagamento caso seja maior que o custo também no for
     //Criar nova variável pra armazenar o resto das divisões (validar se faz sentido criar um método pra incrementar ela ou se no próprio for);
 
-    //Validar quantidade de notas para dar troco, ex: 1 Nota de 100, 5 de 20, 3 de 1 real e assim por diante
 
     //Método para calcular o troco em cédulas
     public double returnTotalNoteChange () {
         Collections.sort(noteList, Collections.reverseOrder());
-
-
-//        returnTotalNoteChange = calculateTotalMoneyChange() / noteList.get(i); INICIO COM 100
-//    i=1, valor=150
-//    i=2, valor=50
-//    i=3, valor
-
-
-        double novoValor;
         for (int i = 0; i < noteList.size(); i++) {
             System.out.println("VALOR DO ELEMENTO DA LISTA NA POSICAO i = " + i + " valor = " + noteList.get(i));
             double validateCalculateTotalMoneyChange = calculateTotalMoneyChange();
             if (validateCalculateTotalMoneyChange == 0) {
                 break;
             }
-            novoValor = returnTotalNoteChange / noteList.get(i); //pega o valor inicial e divide pelo elemento do seu i; novoValor = returnTotalNoteChange /  noteList.get(i);
+            newValueNoteChange = returnTotalNoteChange / noteList.get(i); //pega o valor inicial e divide pelo elemento do seu i;
             double pegaResto = calculateTotalMoneyChange() % noteList.get(i); //resto da divisao.
-            int primeiroAntesDaVirgula = (int) Math.floor(returnTotalNoteChange); //o primeiro valor antes da virgula.
-            if (primeiroAntesDaVirgula > 0) { //A cedula cabe no valor do troco.
-                double teste = primeiroAntesDaVirgula * noteList.get(i); //devo diminuir o valor, pela quantidade que me retornou de inteiro * pelo valor da cedula (calcula a qtd de notas de x valor).
-                returnTotalNoteChange --;//precisa tirar o valor que deu na variavel teste, e atualizar o returnTotalNoteChange para que na proxima iteração o valor que a variavel novoValor receba seja o troco atualizado, ou seja, diminuindo 100 reais, por exemplo.
+            if (eachValueNoteQuantity > 0) { //A cedula cabe no valor do troco.
+                double teste = eachValueNoteQuantity * noteList.get(i); //devo diminuir o valor, pela quantidade que me retornou de inteiro * pelo valor da cedula (calcula a qtd de notas de x valor).
+                returnTotalNoteChange--;//precisa tirar o valor que deu na variavel teste, e atualizar o returnTotalNoteChange para que na proxima iteração o valor que a variavel novoValor receba seja o troco atualizado, ou seja, diminuindo 100 reais, por exemplo.
             }
-            /*
-            * 100/200 = 0.5;
-            * 250/100 = 2.50
-            * 2 nota de 100 e tem resto;
-            * se tem resto, devo diminuir o valor, pela quantidade que me retornou antes do resto(inteiro) * pelo valor da cedula.
-            * Atualizo valor
-            * Divido de novo
-            * 50/100 = 0.5
-            * a nota não pode ser entregue.
-            * pego proxima nota
-            *
-            * */
-            System.out.println("LINHA PARA TESTE E VER SE ISSO ME AJUDA EM ALGO, PFVR ME AJUDA JESUS> " + "resto: " + pegaResto + "primeiro int: " + primeiroAntesDaVirgula);
+            System.out.println("LINHA PARA TESTE E VER SE ISSO ME AJUDA EM ALGO, PFVR ME AJUDA JESUS> " + "resto: " + pegaResto + " primeiro int: " + eachValueNoteQuantity);
             if (returnTotalNoteChange != 0) {
                 System.out.println(returnTotalNoteChange);
             } else {
