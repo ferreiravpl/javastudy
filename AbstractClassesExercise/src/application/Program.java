8b5735a51e3ced35b813d6792e9e73474602c37b
Lucas Ferreira <ferreiravpl@gmail.com>
2023-12-21 13:21:49 -0300

    Criação do programa

--- AbstractClassesExercise/src/application/Program.java
+++ AbstractClassesExercise/src/application/Program.java
@@ -1,14 +1,51 @@
 package application;
 
 import java.util.Scanner;
+
+import entities.Person;
 import entities.PhysicalPerson;
 import entities.LegalPerson;
+
+import java.util.List;
+import java.util.ArrayList;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        List<Person> taxPayers = new ArrayList<>();
 
+        System.out.print("Enter the number of tax payers: ");
+        Integer taxPayersQty = sc.nextInt();
+        sc.nextLine();
 
+        for (int i = 1; i < taxPayersQty; i++) {
+            System.out.println("Tax payer #" + i + " data:");
+            System.out.print("Individual or company (i/c)?");
+            char personType = sc.next().charAt(0);
+            if (personType == 'c') {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Anual income: ");
+                Double anualIncome = sc.nextDouble();
+                System.out.print("Number of employees: ");
+                Integer employeeQty = sc.nextInt();
+                sc.nextLine();
+                taxPayers.add(new LegalPerson(name, anualIncome, employeeQty));
+            } else {
+                System.out.print("Name: ");
+                String name = sc.nextLine();
+                System.out.print("Anual income: ");
+                Double anualIncome = sc.nextDouble();
+                System.out.print("Number of employees: ");
+                Double healthExpenses = sc.nextDouble();
+                taxPayers.add(new PhysicalPerson(name, anualIncome, healthExpenses));
+            }
+        }
+
+        for (Person person : taxPayers) {
+            System.out.println(person);
+        }
         sc.close();
     }
+
 }
