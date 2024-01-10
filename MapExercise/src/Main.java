647853561f992599c44d2bcadda8b8483532b081
Lucas Ferreira <ferreiravpl@gmail.com>
2024-01-10 13:05:15 -0300

    Criando repositório

---
+++ MapExercise/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //https://www.udemy.com/course/java-curso-completo/learn/lecture/10852536#overview
+    }
+}
