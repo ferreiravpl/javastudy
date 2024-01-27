ef5557d1b18fee616c98cf69bb57acb91b002dfb
Lucas Ferreira <ferreiravpl@gmail.com>
2024-01-27 16:34:29 -0300

    Delete values exercise

--- jdbc2/src/application/Main.java
+++ jdbc2/src/application/Main.java
@@ -1,46 +1,32 @@
 package application;
 
 import db.DB;
+import db.DbIntegrityException;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Connection conn;
+        Connection conn = null;
         PreparedStatement st = null;
         try {
             conn = DB.getConnection();
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                            + "VALUES "
-                            + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "DELETE FROM department "
+                            + "WHERE "
+                            + "Id = ?");
 
-            st.setString(1, "Carl Purple");
-            st.setString(2, "carl@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st.setInt(1, 2);
 
             int rowsAffected = st.executeUpdate();
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            } else {
-                System.out.println("No rows affected!");
-            }
+            System.out.println("Done! Rows affected: " + rowsAffected);
 
-        } catch (SQLException | ParseException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            throw new DbIntegrityException(e.getMessage());
         } finally {
             DB.closeStatement(st);
             DB.closeConnection();
