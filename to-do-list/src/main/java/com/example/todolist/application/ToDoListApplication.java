6142dc8afcfb4c0c2173e7a375638e208bd792d6
Lucas Ferreira <ferreiravpl@gmail.com>
2024-04-07 15:24:56 -0300

    Primeira alteração de pacotes

--- to-do-list/src/main/java/com/example/todolist/application/ToDoListApplication.java
+++ to-do-list/src/main/java/com/example/todolist/application/ToDoListApplication.java
@@ -1,4 +1,4 @@
-package com.example.todolist;
+package com.example.todolist.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
